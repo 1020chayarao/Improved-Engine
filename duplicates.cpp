ce7746d1037203f0c0381bdf2e7bbf88c8b720f8
ChayaRao <49594257+1020chayarao@users.noreply.github.com>
2021-06-23 16:31:50 +0530

    duplicates

--- duplicates.cpp
+++ duplicates.cpp
@@ -1,27 +1,20 @@
 /*Given an array nums containing n + 1 integers where each integer 
 is between 1 and n (inclusive), prove that at least one duplicate number must exist.
 Assume that there is only one duplicate number, find the duplicate one.*/
-
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-          if (nums.size() > 1){
-              int slow = nums[0];
-              int fast = nums[nums[0]];
-              while (slow != fast){
-                  slow = nums[slow];
-                  fast = nums[nums[fast]];
-              }
-
-            fast = 0;
-            while (fast != slow){
-                  fast = nums[fast];
-                  slow = nums[slow];
+        unordered_set<int> s;
+        int presize;
+        for(int i=0;i<nums.size();i++)
+        {
+            presize = s.size();
+            s.insert(nums[i]);
+            if(s.size() == presize)
+            {
+                return nums[i];
             }
-            return slow;
         }
         return -1;
     }
-        
-    
 };
