5cfb0643843703ffe870b9de2126086fc757ffff
ChayaRao <49594257+1020chayarao@users.noreply.github.com>
2021-07-04 14:39:16 +0530

    Create all_factors_of_n.cpp

---
+++ all_factors_of_n.cpp
@@ -0,0 +1,15 @@
+vector<int> Solution::allFactors(int A) {
+
+    vector<int> ans;
+    for(int i=1;i<=sqrt(A);i++){
+        if(A%i==0){
+            if (A/i == i)
+                ans.push_back(i);
+            else
+              {ans.push_back(i); 
+              ans.push_back(A/i);}   
+        }
+    }
+    sort(ans.begin(),ans.end());
+    return ans;
+}
